e5828f333675f03d3b0d346a8954a06c7b6bce59
jshan2017 <jshan2017@gmail.com>
2018-06-20 00:53:29 +0900

    fixing redirection parsing

--- syntactic.c
+++ syntactic.c
@@ -42,9 +42,56 @@ DynArray_T parseTokens(DynArray_T tokens){
         }
         
     }
+    int lts,gts;//redirection processing
+    for(int i=0; i<DynArray_getLength(output); i++){
+        lts=0;gts=0;
+        for(int j=0; j<DynArray_getLength(DynArray_get(output,i));j++){
+            if(strchr((DynArray_get(DynArray_get(output,i),j)),'<')){
+                if(i>0 || 
+                    strchr((DynArray_get(DynArray_get(output,i),j)),'<')!=
+                    strrchr((DynArray_get(DynArray_get(output,i),j)),'<')||
+                    lts==1){
+                    fprintf(stderr,"Invalid: Multiple redirection of standard input\n");
+                    return NULL;
+                }
+                if(j==0){
+                    fprintf(stderr, "Invalid: Missing command name\n");
+                    return NULL;
+                }
+                if(DynArray_getLength(DynArray_get(output,i))==j+1){
+                    fprintf(stderr, "Invalid: Standard input redirection without file name\n");
+                    return NULL;
+                }
+
+                lts=1;
+            }
+            
+            if(strchr((DynArray_get(DynArray_get(output,i),j)),'>')){
+                if(DynArray_getLength(output)>i+1 || 
+                    strchr((DynArray_get(DynArray_get(output,i),j)),'>')!=
+                    strrchr((DynArray_get(DynArray_get(output,i),j)),'>')||
+                    gts==1){
+                    fprintf(stderr,"Invalid: Multiple redirection of standard input\n");
+                    return NULL;
+                }
+                if(j==0){
+                    fprintf(stderr, "Invalid: Missing command name\n");
+                    return NULL;
+                }
+                if(DynArray_getLength(DynArray_get(output,i))==j+1){
+                    fprintf(stderr, "Invalid: Standard output redirection without file name\n");
+                    return NULL;
+                }
+                gts=1;
+            }
+           
+           
+               
+        }
+    }
     DynArray_free(tokens);
-    /*this is for debugging
-    printf("syntactic below \n");
+    /*this is for debugging*/
+    printf("syntactic below ################33\n");
     for(int j=0;j<DynArray_getLength(output);j++){
         printf("\n#%d th command :",j);
         for(int i=0;i<DynArray_getLength(DynArray_get(output,j));i++){
@@ -53,7 +100,8 @@ DynArray_T parseTokens(DynArray_T tokens){
         }
         
     }
-    printf("\n");*/
+    printf("\n##############\n");
+    printf("\n");
     
     return output;
 }
